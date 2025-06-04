a1babce035bffc1613247d0a5811ad5a16e37f3f
Izamu165 <koliviraj541@gmail.com>
2025-06-04 14:33:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sachinopboy2/ShizukaMusicBot",
+    "https://github.com/Izamu165/Isamu-smartboy",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
